9ce53385ac8861efa0d779bea479d8e276340773
Magret <belloabiodun17@gmail.com>
2026-06-25 15:09:30 -0230

    Create api for all trackers endpointS

--- src/api/trackersApi.ts
+++ src/api/trackersApi.ts
@@ -7,6 +7,7 @@ if (!BACKEND_URL) {
   );
 }
 
+// get all trackers
 export const getTrackers = async (): Promise<TrackerResponse> => {
   try {
     const token =
@@ -25,12 +26,230 @@ export const getTrackers = async (): Promise<TrackerResponse> => {
     const data = await response.json();
 
     if (!response.ok) {
-      throw new Error(data.message || data.error || "Forgot Password failed");
+      throw new Error(data.message || data.error || "Get Trackers failed");
     }
 
     return data;
   } catch (error) {
-    console.error("Error in forgot password user:", error);
+    console.error("Error in get tracker:", error);
+    throw error;
+  }
+};
+
+// get tracker by id
+export const getTracker = async (
+  id: string
+): Promise<TrackerResponse> => {
+  try {
+    const token =
+      typeof window !== "undefined" ? localStorage.getItem("jotoken") : null;
+
+    const response = await fetch(`${BACKEND_URL}/trackers/:id`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        ...(token && {
+          Authorization: `Bearer ${token}`,
+        }),
+      },
+    });
+
+    const data = await response.json();
+
+    if (!response.ok) {
+      throw new Error(data.message || data.error || "Get Tracker failed");
+    }
+
+    return data;
+  } catch (error) {
+    console.error("Error in get tracker:", error);
+    throw error;
+  }
+};
+
+// get tracker by id
+export const deleteTracker = async (
+  id: string
+): Promise<TrackerResponse> => {
+  try {
+    const token =
+      typeof window !== "undefined" ? localStorage.getItem("jotoken") : null;
+
+    const response = await fetch(`${BACKEND_URL}/trackers/:id`, {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+        ...(token && {
+          Authorization: `Bearer ${token}`,
+        }),
+      },
+    });
+
+    const data = await response.json();
+
+    if (!response.ok) {
+      throw new Error(data.message || data.error || "Delete Tracker failed");
+    }
+
+    return data;
+  } catch (error) {
+    console.error("Error in delete tracker:", error);
+    throw error;
+  }
+};
+
+// update tracker by id
+export const updateTracker = async (
+  id: string
+): Promise<TrackerResponse> => {
+  try {
+    const token =
+      typeof window !== "undefined" ? localStorage.getItem("jotoken") : null;
+
+    const response = await fetch(`${BACKEND_URL}/trackers/:id`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+        ...(token && {
+          Authorization: `Bearer ${token}`,
+        }),
+      },
+    });
+
+    const data = await response.json();
+
+    if (!response.ok) {
+      throw new Error(data.message || data.error || "Update Tracker failed");
+    }
+
+    return data;
+  } catch (error) {
+    console.error("Error in update tracker:", error);
+    throw error;
+  }
+};
+
+// pause tracker by id
+export const pauseTracker = async (
+  id: string
+): Promise<TrackerResponse> => {
+  try {
+    const token =
+      typeof window !== "undefined" ? localStorage.getItem("jotoken") : null;
+
+    const response = await fetch(`${BACKEND_URL}/trackers/:id/pause`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+        ...(token && {
+          Authorization: `Bearer ${token}`,
+        }),
+      },
+    });
+
+    const data = await response.json();
+
+    if (!response.ok) {
+      throw new Error(data.message || data.error || "Pause Tracker failed");
+    }
+
+    return data;
+  } catch (error) {
+    console.error("Error in pause tracker:", error);
+    throw error;
+  }
+};
+
+// resume tracker by id
+export const resumeTracker = async (
+  id: string
+): Promise<TrackerResponse> => {
+  try {
+    const token =
+      typeof window !== "undefined" ? localStorage.getItem("jotoken") : null;
+
+    const response = await fetch(`${BACKEND_URL}/trackers/:id/resume`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+        ...(token && {
+          Authorization: `Bearer ${token}`,
+        }),
+      },
+    });
+
+    const data = await response.json();
+
+    if (!response.ok) {
+      throw new Error(data.message || data.error || "Get Tracker failed");
+    }
+
+    return data;
+  } catch (error) {
+    console.error("Error in get tracker:", error);
+    throw error;
+  }
+};
+
+// check-now
+export const checkNowTracker = async (
+  id: string
+): Promise<TrackerResponse> => {
+  try {
+    const token =
+      typeof window !== "undefined" ? localStorage.getItem("jotoken") : null;
+
+    const response = await fetch(`${BACKEND_URL}/trackers/:id/check-now`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        ...(token && {
+          Authorization: `Bearer ${token}`,
+        }),
+      },
+    });
+
+    const data = await response.json();
+
+    if (!response.ok) {
+      throw new Error(data.message || data.error || "Get Tracker failed");
+    }
+
+    return data;
+  } catch (error) {
+    console.error("Error in get tracker:", error);
+    throw error;
+  }
+}
+
+//post tracker
+export const postTracker = async (
+  payload: any
+): Promise<TrackerResponse> => {
+  try {
+    const token =
+      typeof window !== "undefined" ? localStorage.getItem("jotoken") : null;
+
+    const response = await fetch(`${BACKEND_URL}/trackers`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        ...(token && {
+          Authorization: `Bearer ${token}`,
+        }),
+      },
+      body: JSON.stringify(payload),
+    });
+
+    const data = await response.json();
+
+    if (!response.ok) {
+      throw new Error(data.message || data.error || "Post Tracker failed");
+    }
+
+    return data;
+  } catch (error) {
+    console.error("Error in post tracker:", error);
     throw error;
   }
 };
