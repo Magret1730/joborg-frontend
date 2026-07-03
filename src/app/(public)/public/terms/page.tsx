b0d4fc671a2ff5b9cee6c579a3b093a03561c932
Magret <belloabiodun17@gmail.com>
2026-07-02 23:23:07 -0230

    Create placeholder pages for terms and provacy pages

---
+++ src/app/(public)/public/terms/page.tsx
@@ -0,0 +1,10 @@
+import { UnderConstruction } from "@/components/shared/UnderConstruction";
+
+export default function Terms() {
+  return (
+    <UnderConstruction
+      title="Terms Page is coming soon"
+      description="We're working hard to bring you a way to get in touch with us. Stay tuned for updates!"
+    />
+  );
+}
